82e6a2615ef04e85fe3f7e5db55f30b3a9d21901
현민영 <151743721+95hyun@users.noreply.github.com>
2024-01-05 09:39:45 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+
+        List<Integer> ranking = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            ranking.add(score[i]);
+
+            if (ranking.size() > k) {
+                ranking.remove(Collections.min(ranking));
+            }
+
+            answer[i] = Collections.min(ranking);
+        }
+
+
+        return answer;
+    }
+}
